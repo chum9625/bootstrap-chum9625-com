3c9942e0f9008af562142a8f18d23f886ff957b3
chum9625 <17173386+chum9625@users.noreply.github.com>
2021-10-23 21:47:46 +0900

    pwa実装

---
+++ app.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  console.log("Found serviceWorker");
+  navigator.serviceWorker.register("/sw.js").then((reg) => {
+    console.log("Service Worker Registered", reg);
+  });
+} else {
+  console.log("Not Found serviceWorker");
+}
